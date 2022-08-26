b757e3112ba6d390ccaf6e9e20e8265c7034bf9e
Rogerio Oliveira <roliveira@cyclomedia.com>
2022-08-27 01:20:24 +0200

    added firebase configuration; added google authentication;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
 import Navigation from "./routes/navigation/navigation.component";
