8c1be1f98c9af16bb7a82b703405e83442690325
Rogerio Oliveira <roliveira@cyclomedia.com>
2022-08-27 00:07:10 +0200

    added logo; added styles to shape navigation bar; fixed index styles file;

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,24 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import './navigation.styles.scss';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
-        <div className='links-container'>
+        <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
             SHOP
+          </Link>
+          <Link className='nav-link' to='/contact'>
+            CONTACT
+          </Link>
+          <Link className='nav-link' to='/about'>
+            ABOUT
           </Link>
         </div>
       </div>
