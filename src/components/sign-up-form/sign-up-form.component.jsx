4da44055b449dbbf184a073315f7f9311e215c44
Roger Oliver <roger@v9y.nl>
2022-08-30 14:35:11 +0200

    created a custom button and its style; small fix on custom input style; added new custom button on sign up form;

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
 import './sign-up-form.styles.scss';
 
@@ -51,8 +52,9 @@ const SignUpForm = (props) => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleOnSubmit}>
         <FormInput
           label='Display Name'
@@ -90,7 +92,7 @@ const SignUpForm = (props) => {
           required
         />
 
-        <button type='submit'>Sign Up</button>
+        <Button type='submit' buttonText='Sign Up' />
       </form>
     </div>
   );
