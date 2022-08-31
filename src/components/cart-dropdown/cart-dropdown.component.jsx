006252adbd3f0aac9fce1408be07c4456d821c72
Roger Oliver <roger@v9y.nl>
2022-08-31 16:44:11 +0200

    created a cart item component; improved the cart context to include cartitems;

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,22 @@
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = (props) => {
+
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {
+          cartItems.map(cartItem => {
+            return <CartItem key={cartItem.id} cartItem={cartItem}></CartItem>
+          })
+        }
+      </div>
       <Button buttonText='go to checkout' />
     </div>
   );
