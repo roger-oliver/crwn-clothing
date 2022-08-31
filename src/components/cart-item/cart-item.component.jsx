2f6c5bf3981095f8e6889eb6baac2e0c307c1061
Roger Oliver <roger@v9y.nl>
2022-08-31 18:17:12 +0200

    added cart item styles; fixed some styles on cart dropdown component; reshaped cart item component;

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,14 @@
 import './cart-item.styles.scss';
 
 const CartItem = (props) => {
-  const { name, quantity } = props.cartItem;
+  const { name, imageUrl, price, quantity } = props.cartItem;
   return (
     <div className="cart-item-container">
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+      <img src={imageUrl} alt={name} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className='price'>{quantity} x ${price}</span>
+      </div>
     </div>
   )
 };
