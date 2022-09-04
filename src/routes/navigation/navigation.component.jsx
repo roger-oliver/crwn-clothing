cfdf9e6442b6af60fc9841e3792d3ca73af43719
Rogerio Oliveira <roliveira@cyclomedia.com>
2022-09-04 23:52:25 +0200

    lectures 149, 150, 151, 152, 153, 154 - adding redux and converting userContext; - added redux and react-redux packages; - created user reducer file; - created root-reducer; - created store file; - moved all user action types to a user types file; - created the user action file; - created user selector file; - changed navigation to get currentUser from global store; - removed user context api;

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,16 +2,18 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { CartContext } from '../../contexts/cart.context';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 const Navigation = () => {
 
-  const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
+
+  const currentUser = useSelector(selectCurrentUser);
 
   const handleSigOutUser = async () => {
     await signOutUser();
