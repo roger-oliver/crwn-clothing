5b1dfca54608397ee457b4aa87e35bc28ef0e14f
Roger Oliver <roger@v9y.nl>
2022-09-08 21:56:06 +0200

    lectures 155 and 156 - added redux to categories; - created type, action, reducer, and selector for categories; - added new reducer to root-reducer; changed component categories-preview to use redux; - changed component category to use redux; - removed the categories context and its references;

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { setCategoriesMap } from '../../store/categories/category.action';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import './shop.styles.scss';
 
 const Shop = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesAndItems = async () => {
+      const categoriesMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoriesMap));
+    };
+    getCategoriesAndItems();
+  }, []);
+  
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
