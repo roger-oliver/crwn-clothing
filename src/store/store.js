a14d464bf8f7cbe95c1a37f2136777c389bcec72
Roger Oliver <roger@v9y.nl>
2022-09-14 22:26:47 +0200

    lectures 172, 173, 174 - changing redux-thunk to redux-saga in categories; - installed the redux-saga package; - changed the catetory action removing fetch async and exporting the start, success, and failure actions; - created the category saga file to include the async part of categories; - created the root saga with the categories saga file; - changed shop component to use fetch categories start;

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,20 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
-import { rootReducer } from "./root-reducer";
+import { applyMiddleware, compose, createStore } from 'redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from '@redux-saga/core';
+import { rootReducer } from './root-reducer';
+import { rootSaga } from './root-saga';
 
-const {
-  REACT_APP_ENVIRONMENT,
-} = process.env;
+const { REACT_APP_ENVIRONMENT } = process.env;
 
-const middlewares = [
-  logger, thunk,
-];
+const sagaMiddleware = createSagaMiddleware();
 
-const composedEnhancers = REACT_APP_ENVIRONMENT === 'development' ? compose(applyMiddleware(...middlewares)) : undefined;
+const middlewares = [logger, sagaMiddleware];
 
+const composedEnhancers =
+  REACT_APP_ENVIRONMENT === 'development'
+    ? compose(applyMiddleware(...middlewares))
+    : undefined;
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);
+    
+sagaMiddleware.run(rootSaga);
